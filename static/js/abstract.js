f42b5f29daa69b7b6d614de9a9f1d7b7dd995cbb
ksnyy <jeyyyjeyyyy@gmail.com>
2023-09-11 17:07:37 +0800

    di na gumana chatgpt

---
+++ static/js/abstract.js
@@ -0,0 +1,5 @@
+document.getElementById('generate').addEventListener('click', function() {
+    var spanContainer = document.querySelector('.span-container');
+    spanContainer.classList.remove('hidden');
+
+});
